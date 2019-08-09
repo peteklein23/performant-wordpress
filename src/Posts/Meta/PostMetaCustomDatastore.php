bbc28d8ff0553d71e5eab901140ecebadf054bc0
Pete Klein <pklein@ackmanndickenson.com>
2019-08-08 21:03:16 -0500

    fixed variable name

--- src/Posts/Meta/PostMetaCustomDatastore.php
+++ src/Posts/Meta/PostMetaCustomDatastore.php
@@ -7,9 +7,10 @@ use Carbon_Fields\Field\Field;
 use Carbon_Fields\Datastore\Post_Meta_Datastore;
 
 /**
-  * Prevents prefixing field keys with "_"
-  */
-class PostMetaCustomDatastore extends Post_Meta_Datastore {
+ * Prevents prefixing field keys with "_"
+ */
+class PostMetaCustomDatastore extends Post_Meta_Datastore
+{
 
     /**
      * Removes the prefix
@@ -17,7 +18,8 @@ class PostMetaCustomDatastore extends Post_Meta_Datastore {
      * @param string $storageKey
      * @return void
      */
-    private function stripPrefixFromStorageKey(string $storageKey){
+    private function stripPrefixFromStorageKey(string $storageKey)
+    {
         $pos = strpos($storageKey, Key_Toolset::KEY_PREFIX);
 
         return substr_replace($storageKey, '', $pos, strlen(Key_Toolset::KEY_PREFIX));
@@ -29,7 +31,8 @@ class PostMetaCustomDatastore extends Post_Meta_Datastore {
      * @param string $storageKey
      * @return void
      */
-    private function addPrefixToStorageKey(string $storageKey){
+    private function addPrefixToStorageKey(string $storageKey)
+    {
         return Key_Toolset::KEY_PREFIX . $storageKey;
     }
 
@@ -39,9 +42,10 @@ class PostMetaCustomDatastore extends Post_Meta_Datastore {
      * @param array $storageKeyPatterns
      * @return void
      */
-    private function stripPrefixFromStorageKeyPatterns(array $storageKeyPatterns){
+    private function stripPrefixFromStorageKeyPatterns(array $storageKeyPatterns)
+    {
         $updatedStorageKeyPatterns = [];
-        foreach($storageKeyPatterns as $key => $pattern){
+        foreach ($storageKeyPatterns as $key => $pattern) {
             $updatedKey = $this->stripPrefixFromStorageKey($key);
             $updatedStorageKeyPatterns[$updatedKey] = $pattern;
         }
@@ -55,9 +59,10 @@ class PostMetaCustomDatastore extends Post_Meta_Datastore {
      * @param array $cascadingStorageArray
      * @return void
      */
-    private function addPrefixToCascadingStorageArray(array $cascadingStorageArray){
+    private function addPrefixToCascadingStorageArray(array $cascadingStorageArray)
+    {
         $updatedArray = [];
-        foreach($cascadingStorageArray as $item){
+        foreach ($cascadingStorageArray as $item) {
             $updatedKey = $this->addPrefixToStorageKey($item->key);
             $obj = new \stdClass();
             $obj->key = $updatedKey;
@@ -67,58 +72,60 @@ class PostMetaCustomDatastore extends Post_Meta_Datastore {
 
         return $updatedArray;
     }
-        
-    /**
-	 * Get the field value(s)
-	 *
-	 * @param Field $field The field to get value(s) for
-	 * @return null|array<array>
-	 */
-	public function load( Field $field ) {
-		$storage_key_patterns = $this->key_toolset->get_storage_key_getter_patterns( $field->is_simple_root_field(), $this->get_full_hierarchy_for_field( $field ) );
-        $storage_key_patterns = $this->stripPrefixFromStorageKeyPatterns($storage_key_patterns);
-        $cascading_storage_array = $this->get_storage_array( $field, $storage_key_patterns );
-        $cascading_storage_array = $this->addPrefixToCascadingStorageArray($cascading_storage_array);
-        $value_tree_array = $this->cascading_storage_array_to_value_tree_array( $cascading_storage_array );
-		if ( $value_tree_array === null ) {
-			return $value_tree_array;
-		}
-        $value_tree = $this->value_tree_array_to_value_tree( $value_tree_array, $field );
-        
-		return $value_tree;
-    }
-    
-    /**
-	 * Save the field value(s)
-	 *
-	 * @param Field $field The field to save.
-	 */
-	public function save( Field $field ) {
-		$value_set = $field->get_full_value();
 
-		if ( empty( $value_set ) && $field->get_value_set()->keepalive() ) {
-			$storage_key = $this->key_toolset->get_storage_key(
-				$field->is_simple_root_field(),
-				$this->get_full_hierarchy_for_field( $field ),
-				$this->get_full_hierarchy_index_for_field( $field ),
-				0,
-				$this->key_toolset->get_keepalive_property()
+    /**
+     * Get the field value(s)
+     *
+     * @param Field $field The field to get value(s) for
+     * @return null|array<array>
+     */
+    public function load(Field $field)
+    {
+        $storage_key_patterns = $this->key_toolset->get_storage_key_getter_patterns($field->is_simple_root_field(), $this->get_full_hierarchy_for_field($field));
+        $storage_key_patterns = $this->stripPrefixFromStorageKeyPatterns($storage_key_patterns);
+        $cascading_storage_array = $this->get_storage_array($field, $storage_key_patterns);
+        $cascading_storage_array = $this->addPrefixToCascadingStorageArray($cascading_storage_array);
+        $value_tree_array = $this->cascading_storage_array_to_value_tree_array($cascading_storage_array);
+        if ($value_tree_array === null) {
+            return $value_tree_array;
+        }
+        $value_tree = $this->value_tree_array_to_value_tree($value_tree_array, $field);
+
+        return $value_tree;
+    }
+
+    /**
+     * Save the field value(s)
+     *
+     * @param Field $field The field to save.
+     */
+    public function save(Field $field)
+    {
+        $value_set = $field->get_full_value();
+
+        if (empty($value_set) && $field->get_value_set()->keepalive()) {
+            $storage_key = $this->key_toolset->get_storage_key(
+                $field->is_simple_root_field(),
+                $this->get_full_hierarchy_for_field($field),
+                $this->get_full_hierarchy_index_for_field($field),
+                0,
+                $this->key_toolset->get_keepalive_property()
             );
-            $storage_key = $this->stripPrefixFromStorageKey($storageKey);
-			$this->save_key_value_pair( $storage_key, '' );
-		}
-		foreach ( $value_set as $value_group_index => $values ) {
-			foreach ( $values as $property => $value ) {
-				$storage_key = $this->key_toolset->get_storage_key(
-					$field->is_simple_root_field(),
-					$this->get_full_hierarchy_for_field( $field ),
-					$this->get_full_hierarchy_index_for_field( $field ),
-					$value_group_index,
-					$property
+            $storage_key = $this->stripPrefixFromStorageKey($storage_key);
+            $this->save_key_value_pair($storage_key, '');
+        }
+        foreach ($value_set as $value_group_index => $values) {
+            foreach ($values as $property => $value) {
+                $storage_key = $this->key_toolset->get_storage_key(
+                    $field->is_simple_root_field(),
+                    $this->get_full_hierarchy_for_field($field),
+                    $this->get_full_hierarchy_index_for_field($field),
+                    $value_group_index,
+                    $property
                 );
                 $storage_key = $this->stripPrefixFromStorageKey($storage_key);
-				$this->save_key_value_pair( $storage_key, $value );
-			}
-		}
-	}
+                $this->save_key_value_pair($storage_key, $value);
+            }
+        }
+    }
 }
